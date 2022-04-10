a448daab0d25367b441dfdde34e545e5702f0cc8
phamdat <phamdat2082k@gmail.com>
2022-04-10 10:40:00 +0700

    add new a tiktok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'
 function App() {
+
+  //componet đc render lại sau khi setCounter
+  //setState gọi callback 
+  /*
+  const [counter,setCounter] = useState(1)
+
+  const handleCounter = ()=>{
+     // setCounter(counter + 1)
+        setCounter(prevState => prevState + 1)
+  }
+  return (
+    <div className="App" style={{padding : 20}}>
+        <h1>{counter}</h1>
+        <button onClick={handleCounter}>Increase</button>
+    </div>
+  );*/
+
+  const [info ,setInfo] = useState ({
+    name: 'Nguyen Van A',
+    age: 18,
+    address: 'Ha Noi, VN'
+
+  })
+  const handleUpdate = () => {
+    setInfo({
+      ...info,
+      bio: 'Yeu mau hong'
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate}>Update</button>
     </div>
   );
+
 }
 
 export default App;
+
+
